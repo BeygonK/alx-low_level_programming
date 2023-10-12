44ca6a1a8d900c070662b97243ab043e6724522c
BeygonK <kipkoechbeygon@gmail.com>
2023-10-12 09:45:44 -0700

    changed

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * void print_alphabet_x10(void) - Print alphabets 10 times
+ * print_alphabet_x10(void) - Print alphabets 10 times
  * Return: 0
  */
 void print_alphabet_x10(void)
