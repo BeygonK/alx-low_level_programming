8c067be184e361a17e6d1b2e3ab60761c1f9f3f0
BeygonK <kipkoechbeygon@gmail.com>
2023-10-12 12:01:50 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last  digit
+ * @a: parameter
+ * Return: 0
+ */
+int print_last_digit(int a)
+{
+	int b;
+	
+	b = a % 10;
+	return (b);
+}
