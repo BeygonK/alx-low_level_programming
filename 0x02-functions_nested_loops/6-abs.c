ef774df0680dbd5dc3411279678050e02c79b359
BeygonK <kipkoechbeygon@gmail.com>
2023-10-12 11:53:11 -0700

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
  * _abs - prints absolute value
+ * @a: parameter
  * Return: 0;
  *
  */
 
-int _abs(int)
+int _abs(int a)
 {
-	return abs(int);
+	return abs(a);
 	return (0);
 	_putchar('\n');
 }
