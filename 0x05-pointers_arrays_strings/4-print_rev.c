3b424b08818485198cd1b13419dd3fe8d239e2a0
BeygonK <kipkoechbeygon@gmail.com>
2023-10-16 03:35:18 -0700

    Revrse a string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_rev - Prints string in revrse
+ * @s: param
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+	int length, i;
+
+	length = 0;
+	while (*s != '\n')
+	{
+		length++;
+		s++;
+	}
+
+	for (i = length - 1; i >= 0; i--)
+	{
+		putchar(s[i]);
+	}
+	putchar('\n');
+}
