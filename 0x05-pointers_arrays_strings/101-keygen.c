6959d69c5367b1709c49365d06eb8c7c73f119e5
BeygonK <kipkoechbeygon@gmail.com>
2023-10-16 07:22:58 -0700

    pasword

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,21 +3,22 @@
 #include <time.h>
 
 int main() {
-    srand(time(0)); // Seed the random number generator with the current time
-
+    int i;
     char password[5]; // Array to hold the password, +1 for the null terminator
 
-    // Generate the first 3 characters
-    for (int i = 0; i < 3; i++) {
-        password[i] = 'A' + rand() % 26; // Generate a random uppercase letter
+    srand(time(0)); /* Seed the random number generator with the current time */
+
+    /* Generate the first 3 characters */
+    for (i = 0; i < 3; i++) {
+        password[i] = 'A' + rand() % 26; /* Generate a random uppercase letter */
     }
 
-    // Generate the last character
-    password[3] = '0' + rand() % 10; // Generate a random number
+    /* Generate the last character */
+    password[3] = '0' + rand() % 10; /* Generate a random number */
 
-    password[4] = '\0'; // Null terminate the password
+    password[4] = '\0'; /* Null terminate the password */
 
-    printf("%s\n", password); // Print the password
+    printf("%s\n", password); /* Print the password */
 
     return 0;
 }
