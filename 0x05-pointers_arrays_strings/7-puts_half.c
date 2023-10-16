f2ab2d530c36356960b9cfe6e5d98a54f4992dbd
BeygonK <kipkoechbeygon@gmail.com>
2023-10-16 04:38:49 -0700

    half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * puts_half - func
+ * @str: param
+ * Return: 0;
+ */
+void puts_half(char *str)
+{
+	int length, start_index, i;
+
+	if (length % 2 == 0)
+	{
+		start_index = length / 2;
+	}
+	else
+	{
+		start_index = (length - 1) / 2;
+	}
+
+	for (i = start_index; i < length; i++)
+	{
+		printf("%c", str[i]);
+	}
+	printf("\n");
+}
