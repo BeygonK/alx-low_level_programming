8b5f23b09a139ac7baea8f611048be5752b018a8
BeygonK <kipkoechbeygon@gmail.com>
2023-10-16 02:58:36 -0700

    print to stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - prints to the output
+ * @str: param
+ * Return: 0
+ */
+void _puts(char *str)
+{
+	while(*str != '\0')
+	{
+		putchar(*str);
+		str++
+	}
+	
+	putchar('\n');
+}
