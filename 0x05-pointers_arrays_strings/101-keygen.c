348f56e57bbc47934f698c183f75d534938f0d8e
BeygonK <kipkoechbeygon@gmail.com>
2023-10-16 07:16:23 -0700

    pasword

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,31 +1,27 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
 #define PASSWORD_LENGTH 10
-/**
- * generatePassword - Function to generate passeord
- * @password: param
- * @length: param
- * Return: 0
- */
-void generatePassword(char *password, int length)
-{
+
+void generatePassword(char *password, int length) {
+    char characters[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 	int i;
-	char characters[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-	for (i = 0; i < length; ++i)
-	{
-		password[i] = characters[rand() % (sizeof(characters) - 1)];
-	}
-	password[length] = '\0';
+    for (i = 0; i < length; ++i) {
+        password[i] = characters[rand() % (sizeof(characters) - 1)];
+    }
 
+    password[length] = '\0'; // Null-terminate the password
 }
 
-int main()
-{
-	srand(time(NULL));
+int main() {
+    srand(time(NULL)); // Seed the random number generator with the current time
 
-	generatePassword(password, PASSWORD_LENGTH);
-	printf("Generated: %s\n", password);
+    char password[PASSWORD_LENGTH + 1]; // +1 for the null terminator
+    generatePassword(password, PASSWORD_LENGTH);
 
-	return (0);
+    printf("Generated Password: %s\n", password);
+
+    return 0;
 }
+
