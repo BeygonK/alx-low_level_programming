6e8d1bcc03ed4b7493aad5aa750ab8e9b7702d34
BeygonK <kipkoechbeygon@gmail.com>
2023-10-19 03:06:49 -0700

    Rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * rot13 - Encodes a string
+ * @str: string to be encoded
+ *
+ * Return: POinter to encoded sring
+ */
+char *rot13(char *str)
+{
+	int i;
+	int j;
+	char s1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char s2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == s1[j])
+			{
+				s[i] = s2[j];
+				break;
+			}	
+		}
+	}
+	return (s);
+}
