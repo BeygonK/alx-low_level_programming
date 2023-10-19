016cca8b831cd43c25d7fb55192c7a1b048ea5ab
BeygonK <kipkoechbeygon@gmail.com>
2023-10-18 22:57:34 -0700

    Another conc

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strncat - Concatenate a string
+ * @dest: First string
+ * @src: Second string
+ * @n: integer
+ * Return: 0
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	return (strncat(dest, src, n));
+
+	return (0)
+}
