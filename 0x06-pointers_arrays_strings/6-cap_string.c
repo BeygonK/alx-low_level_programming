7dcf2a6bd4117f4145633bf63286f2a5eddcd80a
BeygonK <kipkoechbeygon@gmail.com>
2023-10-19 02:00:18 -0700

    Capitalize

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,9 +11,9 @@ char *cap_string(char *str)
 	capitalize = 1;
 	for (int i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == ',' ||
-				str[i] == ';' || str[i] == '.' || str[i] == '!' || str[i] == '?' ||
-				str[i] == '"' || str[i] == '(' || str[i] == ')' || str[i] == '{')
+		if (str[i-1] == ' ' || str[i-1] == '\t' || str[i-1] == '\n' || str[i-1] == ',' ||
+				str[i-1] == ';' || str[i-1] == '.' || str[i-1] == '!' || str[i-1] == '?' ||
+				str[i-1] == '"' || str[i-1] == '(' || str[i-1] == ')' || str[i-1] == '{')
 		{
 			capitalize = 1;
 		}
