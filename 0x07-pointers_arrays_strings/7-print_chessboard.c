baa519fae950b0a2bb32a2c3e98572c5e758d5d5
BeygonK <kipkoechbeygon@gmail.com>
2023-10-25 11:19:02 -0700

    chess

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,14 +1,16 @@
 #include "main.h"
+
 /**
- * print_chessboard - Prints
- * @a: 2D arrau
- * Return: 0
+ * print_chessboard - print chess
+ * @a: param
+ *
+ * Return: void
  */
 void print_chessboard(char (*a)[8])
 {
 	int i, j;
 
-	for (i = 0; i < 8; i++;)
+	for ( i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
 		{
