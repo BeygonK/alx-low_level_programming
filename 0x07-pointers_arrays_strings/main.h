d0e77526fa9d81eeb920d65f9ca95ee0c085e9ec
BeygonK <kipkoechbeygon@gmail.com>
2023-10-25 11:44:19 -0700

    string

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
+void set_string(char **s, char *to);
+
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
