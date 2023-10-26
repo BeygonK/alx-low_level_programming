7500c792c0d885435e9dae90e853c711bebf09d9
BeygonK <kipkoechbeygon@gmail.com>
2023-10-26 09:28:22 -0700

    square root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - Finds the square root
+ * @n: Number to find square root
+ *
+ * Return: Square root
+ */
+int _sqrt_recursion(int n)
+{
+	double res;
+
+	if (n < 0)
+		return (-1);
+	result = sqrt(n);
+	if (result * result == n)
+		return ((int)result);
+	else
+		return (-1);
+}
