d5405ffe6d5d1f52e41354754bef1321c6344818
BeygonK <kipkoechbeygon@gmail.com>
2023-11-01 06:23:43 -0700

    concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -7,12 +7,13 @@
  * @s1: param
  * @s2: second param
  *
- * Return - pointer
+ * Return: pointer
  */
 char *str_concat(char *s1, char *s2)
 {
 	int len1 = 0, len2 = 0, i, j;
-	char *concatenated
+	char *ptr;
+
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
@@ -20,21 +21,25 @@ char *str_concat(char *s1, char *s2)
 
 	while (s1[len1] != '\0')
 		len1++;
-
-	while (s2[len2] != '\0')
+	while (s2[len2 != '\0'])
 		len2++;
-	concatenated = (char *)malloc((len1 + len2 +  1) * sizeof(char));
-	if (concatenated == NULL)
+	ptr = malloc(sizeof(char) * (len1 + len2 + 1));
+
+	if (ptr == NULL)
+	{
+		free(ptr);
 		return (NULL);
+	}
 
-	for (i = 0; i < len1; i++)
-		concatenated[i] = s1[i];
+	for (i = 0; i < len1; i++;)
+	{
+		ptr[i] = s[i];
+	}
 
-	for (j = 0; j < len2; j++)
-		concatenated[i + j] = s2[j];
-
-	concatenated[i + j] = '\0';
-
-	return (concatenated);
-
+	for (j = 0; j <= len2; j++)
+	{
+		ptr[i] = s2[j];
+		i++;
+	}
+	return (ptr);
 }
