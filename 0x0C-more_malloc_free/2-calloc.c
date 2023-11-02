05055368eae7772f7083be1e5c898ec2981c1cb5
BeygonK <kipkoechbeygon@gmail.com>
2023-11-02 02:12:49 -0700

    another chabge

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,7 +12,8 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *ptr;
-	unsigned int total_size, i;
+	unsigned int total_size;
+	size_t i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
