5cb131c1a3728798ebcf106d58f601563d144f67
BeygonK <kipkoechbeygon@gmail.com>
2023-11-02 02:33:58 -0700

    mul

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * is_digit - Checks if a string consists only of digits.
+ * @str: The input string to be checked.
+ *
+ * Return: 1 if all characters are digits, 0 otherwise.
+ */
+int is_digit(char *str)
+{
+	while (*str)
+	{
+		if (*str < '0' || *str > '9')
+		{
+			return (0);
+		}
+		str++;
+	}
+	return (1);
+}
+
+/**
+ * multiply - Multiplies two integers.
+ * @num1: The first integer.
+ * @num2: The second integer.
+ *
+ * Return: The product of num1 and num2.
+ */
+int multiply(int num1, int num2)
+{
+	return (num1 * num2);
+}
+
+/**
+ * main - Entry point of the program.
+ * @argc: The number of command-line arguments.
+ * @argv: An array containing the command-line arguments.
+ *
+ * Return: 0 on success, 98 on incorrect number of arguments or non-digit inputs.
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (98);
+	}
+
+	if (!is_digit(argv[1]) || !is_digit(argv[2]))
+	{
+		printf("Error\n");
+		return (98);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = multiply(num1, num2);
+	printf("%d\n", result);
+	return (0);
+}
