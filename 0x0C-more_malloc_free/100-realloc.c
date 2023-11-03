97429f221bb8e8bac03cb2f6cb538c77e738f9ef
BeygonK <kipkoechbeygon@gmail.com>
2023-11-03 00:43:14 -0700

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -11,28 +11,25 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *new_ptr;
+	void *new_ptr;
 
-	if (new_size == old_size)
-		return (ptr);
-
-	if (ptr == NULL)
-		return (malloc(new_size));
-
-	if (new_size == NULL)
+	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
 
-	new_ptr = malloc(new_size);
+	if (ptr == NULL)
+	{
+		new_ptr = malloc(new_size);
+		if(new_ptr == NULL)
+			return (NULL);
+		return (new_ptr);
+	}
 
-	if (new_ptr == NULL)
+	new_ptr = realloc(ptr, new_size);
+	if(new_ptr == NULL)
 		return (NULL);
-
-	memcpy(new_ptr, ptr, (old_size < new_size ? old_size : new_size));
-
-	free(ptr);
 
 	return (new_ptr);
 }
