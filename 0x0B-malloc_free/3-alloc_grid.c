7500d3d917c77bae31ead1f6ed91011b4e9ca56f
BeygonK <kipkoechbeygon@gmail.com>
2023-11-10 01:08:27 -0800

    Readme

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,19 +11,19 @@ int **alloc_grid(int width, int height)
 {
 	int **grid;
 	int i, j;
-	
+
 	if (width <= 0 || height <= 0)
 	{
 		return (NULL);
 	}
-	
+
 	grid = (int **)malloc(height * sizeof(int *));
-	
+
 	if (grid == NULL)
 	{
 		return (NULL);
 	}
-	
+
 	for (i = 0; i < height; i++)
 	{
 		grid[i] = (int *)malloc(width * sizeof(int));
@@ -36,7 +36,7 @@ int **alloc_grid(int width, int height)
 			free(grid);
 			return (NULL);
 		}
-		
+
 		for (j = 0; j < width; j++)
 		{
 			grid[i][j] = 0;
