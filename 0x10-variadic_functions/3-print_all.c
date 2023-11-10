6f014e5a6a171100afbc75b8409ebc9db87cd3b7
BeygonK <kipkoechbeygon@gmail.com>
2023-11-10 04:01:59 -0800

    print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,60 +1,52 @@
-#include <stdarg.h>
 #include <stdio.h>
+#include <stdarg.h>
 #include <stddef.h>
+
 /**
- * print_all - Prints anything based on the provided format
- * @format: A list of types of arguments passed to the function
- *          c: char
- *          i: integer
- *          f: float
- *          s: char * (if the string is NULL, print (nil) instead)
- *          any other char should be ignored
+ * print_all - Prints all arguments
+ * @format: String
  */
+
 void print_all(const char * const format, ...)
 {
 	va_list args;
-	unsigned int i = 0;
-	char *str;
-	char current_format;
+	int i = 0;
+	char *s;
+	double d;
+	char c;
 
 	va_start(args, format);
 
 	while (format && format[i])
 	{
-		current_format = format[i];
+		switch (format[i++])
+		{
+			case 'c':
+				c = (char)va_arg(args, int);
+				printf("%c", c);
+				break;
+			case 'i':
+				printf("%d", va_arg(args, int));
+				break;
+			case 'f':
+				d = va_arg(args, double);
+				printf("%f", d);
+				break;
+			case 's':
+				s = va_arg(args, char *);
+				if (s == NULL)
+					printf("(nil)");
+				else
+					printf("%s", s);
+				break;
+			default:
+				continue;
+		}
 
-		if (current_format == 'c')
-		{
-			printf("%c", va_arg(args, int));
-		}
-		else if (current_format == 'i')
-		{
-			printf("%d", va_arg(args, int));
-		}
-		else if (current_format == 'f')
-		{
-			printf("%f", va_arg(args, double));
-		}
-		else if (current_format == 's')
-		{
-			str = va_arg(args, char *);
-			if (str != NULL)
-				printf("%s", str);
-			else
-				printf("(nil)");
-		}
-
-		i++;
 		if (format[i])
-		{
-			if (current_format == 'c' || current_format == 'i' ||
-					current_format == 'f' || current_format == 's')
-				printf(", ");
-		}
+			printf(", ");
 	}
 
 	printf("\n");
-
 	va_end(args);
 }
-
