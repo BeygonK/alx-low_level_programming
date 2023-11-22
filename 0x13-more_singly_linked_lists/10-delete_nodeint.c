c5171613dd970ba2294d8f97d2eeece48d218c2e
BeygonK <kipkoechbeygon@gmail.com>
2023-11-22 11:37:41 -0800

    Deletes a node

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -28,7 +28,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	while (prev != NULL && count < index - 1)
 	{
 		prev = prev->next;
-		count++
+		count++;
 	}
 
 	if (prev == NULL || prev->next == NULL)
