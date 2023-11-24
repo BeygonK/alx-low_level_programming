f8330fb08c1256bc4915365856e4e5b29e53a30d
BeygonK <kipkoechbeygon@gmail.com>
2023-11-24 00:19:11 -0800

    prints binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_binary - prints binary
+ * @n: param
+ *
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int mask = 1UL << 63;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	while (mask > 0)
+	{
+		if (n & mask)
+			_putchar('1');
+		else
+			_putchar('0');
+
+		mask >>= 1;
+	}
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stddef.h>
+
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
