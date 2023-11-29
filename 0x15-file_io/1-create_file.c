2ef59deb49aa685f204b37172436b08e4065d6a0
BeygonK <kipkoechbeygon@gmail.com>
2023-11-28 23:20:53 -0800

    changed

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,24 +8,29 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	FILE *file;
+	int fd;
+	ssize_t n;
 
 	if (filename == NULL)
 		return (-1);
 
-	file = fopen(filename, "w");
-	if (file == NULL)
-		return(-1);
-
-	if (text_content != NULL && fputs(text_content, file) == EOF)
-	{
-		fclose(file);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
 		return (-1);
+
+	if (text_content != NULL)
+	{
+		n = write(fd, text_content, strlen(text_content));
+		if (n == -1)
+		{
+			close(fd);
+			return (-1);
+		}
 	}
 
-	if (fclose(file) == EOF)
+	if (close(fd) == -1)
 		return (-1);
 
 	return (1);
-}
 
+}
