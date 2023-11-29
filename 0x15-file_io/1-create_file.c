75a646529ca7b932e7bfeb620eb828277c283784
BeygonK <kipkoechbeygon@gmail.com>
2023-11-28 22:49:33 -0800

    Fixed betty

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,5 @@
 #include "main.h"
-/***
+/**
  * create_file - creates a file and write content
  * @filename: the name of the file
  * @text_content: string
@@ -9,7 +9,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	int file, result;
-	mode_t file_perm = S_IRUSR | S_IWUSR;
+	mode_t file_perm = S_IWUSR;
 
 	if (filename == NULL)
 		return (-1);
