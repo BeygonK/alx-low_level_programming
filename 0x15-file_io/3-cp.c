84c9f7552e374f0437711c70952f348f6d05fd3b
BeygonK <kipkoechbeygon@gmail.com>
2023-11-29 00:20:04 -0800

    Fixed errors

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,18 +9,43 @@
 #define BUFFER_SIZE 1024
 
 /**
- * print_error - prints error
- * @exit_code: the exit code
- * @format: format str
+ * copy_file - cp files
+ * @file_from: source
+ * @file_to: dest
+ *
+ * Return: (1);
  */
-void print_error(int exit_code, const char *format, ...)
-{
-	va_list args;
 
-	va_start(args, format);
-	dprintf(STDERR_FILENO, format, args);
-	va_end(args);
-	exit(exit_code);
+int copy_file(int file_from, int file_to)
+{
+	char buffer[BUFFER_SIZE];
+	ssize_t n;
+
+	while ((n = read(file_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		if (write(file_to, buffer, n) != n)
+		{
+			close(file_from);
+			close(file_to);
+			dprintf(STDERR_FILENO, "Error: Can't write to\n");
+			exit(99);
+		}
+	}
+
+	if (n == -1)
+	{
+		close(file_from);
+		close(file_to);
+		dprintf(STDERR_FILENO, "Error: Can't read from file\n");
+		exit(98);
+	}
+
+	if (close(file_from) == -1 || close(file_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close file %d\n", file_from);
+		exit(100);
+	}
+	return (1);
 }
 
 /**
@@ -32,33 +57,16 @@ void print_error(int exit_code, const char *format, ...)
  */
 int main(int argc, char *argv[])
 {
-	int file_from, file_to, bytes_r, bytes_w;
-	char buffer[BUFFER_SIZE];
-	mode_t file_permissions = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+	int file_from, file_to;
 
 	if (argc != 3)
-		print_error(97, "Usage: %s file_from file_to\n", argv[0]);
-
-	file_from = open(argv[1], O_RDONLY);
-	if (file_from == -1)
-		print_error(98, "Can't read from file %s\n", argv[1]);
-
-	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, file_permissions);
-	if (file_to == -1)
-		print_error(99, "Error:can't write to file %s\n", argv[2]);
-
-	while ((bytes_r = read(file_from, buffer, BUFFER_SIZE)) > 0)
 	{
-		bytes_w = write(file_to, buffer, bytes_r);
-		if (bytes_w == -1)
-			print_error(99, "Error:Can't write to file %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
 	}
-	if (bytes_r == -1)
-		print_error(98, "Error: Can't read from file %s\n", argv[1]);
-	if (close(file_from) == -1)
-		print_error(100, "Error: Can't close fd %d\n", file_from);
-	if (close(file_to) == -1)
-		print_error(100, "Error: Can't close fd %d\n", file_to);
 
-	return (0);
+	file_from = open(argv[0], O_RDONLY);
+	file_to = open(argv[1], O_WRONLY | O_CREAT | O_TRUNC,
+			S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	return (copy_file(file_from, file_to));
 }
