6e6b6c69746907c92b55d12e722007496d2397c0
BeygonK <kipkoechbeygon@gmail.com>
2023-12-02 02:58:57 -0800

    Fixed eerrors

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,72 +1,82 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdarg.h>
-#include <fcntl.h>
-#include <unistd.h>
+#include "main.h"
 #include <sys/types.h>
 #include <sys/stat.h>
 
 #define BUFFER_SIZE 1024
 
 /**
- * copy_file - cp files
- * @file_from: source
- * @file_to: dest
- *
- * Return: (1);
+ * displayUsage - displays usage message
  */
-
-int copy_file(int file_from, int file_to)
+void displayUsage(void)
 {
-	char buffer[BUFFER_SIZE];
-	ssize_t n;
-
-	while ((n = read(file_from, buffer, BUFFER_SIZE)) > 0)
-	{
-		if (write(file_to, buffer, n) != n)
-		{
-			close(file_from);
-			close(file_to);
-			dprintf(STDERR_FILENO, "Error: Can't write to\n");
-			exit(99);
-		}
-	}
-
-	if (n == -1)
-	{
-		close(file_from);
-		close(file_to);
-		dprintf(STDERR_FILENO, "Error: Can't read from file\n");
-		exit(98);
-	}
-
-	if (close(file_from) == -1 || close(file_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close file %d\n", file_from);
-		exit(100);
-	}
-	return (1);
+	dprintf(2, "Usage: cp file_from file_to\n");
+	exit(97);
 }
 
 /**
- * main - copies file
- * @argc: cmd args
- * @argv: array
+ * displayError - display error mesg
+ * @code: error code
+ * @filename: filename
  *
- * Return: 0
+ * Return: void
+ */
+void displayError(int code, const char *filename)
+{
+	switch (code)
+	{
+		case 98:
+			dprintf(2, "Error: Can't read from file %s\n", filename);
+			break;
+		case 99:
+			dprintf(2, "Error: Can't write to %s\n", filename);
+			break;
+		case 100:
+			dprintf(2, "Error: Can't close fd %s\n", filename);
+			break;
+		default:
+			break;
+	}
+	exit(code);
+}
+
+/**
+ * main - entry point
+ * @argc: No. of args
+ * @argv: Array of args
+ * Return: 0 on succes
  */
 int main(int argc, char *argv[])
 {
-	int file_from, file_to;
+	int fd_from, fd_to;
+	char buffer[BUFFER_SIZE];
+	ssize_t bytesRead, bytesWritten;
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		displayUsage();
 
-	file_from = open(argv[0], O_RDONLY);
-	file_to = open(argv[1], O_WRONLY | O_CREAT | O_TRUNC,
-			S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-	return (copy_file(file_from, file_to));
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+		displayError(98, argv[1]);
+
+	fd_to = open(argv[2], O_WRONLY |  O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd_to == -1)
+		displayError(99, argv[2]);
+
+	while ((bytesRead = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		bytesWritten = write(fd_to, buffer, bytesRead);
+
+		if (bytesWritten != bytesRead)
+			displayError(99, argv[2]);
+	}
+	if (bytesRead == -1)
+		displayError(100, argv[1]);
+
+	if (close(fd_from) == -1)
+		displayError(100, argv[1]);
+
+	if (close(fd_to) == -1)
+		displayError(100, argv[2]);
+
+	return (0);
 }
