916eaed9632ebdb012f151ec4042350b9cc90d53
BeygonK <kipkoechbeygone@gmail.com>
2024-05-16 08:42:51 +0300

    fixed

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -16,14 +16,12 @@ int linear_search(int *array, size_t size, int value)
 
 	if (array == NULL)
 		return (-1);
-	else
+
+	for (i = 0; i < size; i++)
 	{
-		for (i = 0; i < size; i++)
-		{
-			printf("Value checked array[%d] = [%d]\n", i, array[i]);
-			if (array[i] == value)
-				return (i);
-		}
-		return (-1);
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
 	}
+	return (-1);
 }
