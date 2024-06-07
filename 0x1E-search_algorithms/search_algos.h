e0c313990d7553ab83665f013b3e42a8e06b6650
BeygonK <kipkoechbeygone@gmail.com>
2024-06-07 12:42:58 +0300

    algis

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,6 +1,8 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef ALGO_H
+#define ALGO_H
+
 #include <stddef.h>
+#include <stdlib.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
